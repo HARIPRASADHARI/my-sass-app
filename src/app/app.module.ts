a2061b9a93a43e2ed883f66d4a00402cb0b0248e
harivirtual <hariprasad@virtual.clinic>
2018-06-21 19:51:23 +0530

    ng 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{FormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialAppModule } from './ngmaterial.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule, MaterialAppModule
+    BrowserModule, BrowserAnimationsModule, MaterialAppModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ngmaterial.module.ts
+++ src/app/ngmaterial.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {  MatButtonModule } from '@angular/material';
+import {  MatButtonModule,MatInputModule,MatDividerModule } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule],
-  exports: [MatButtonModule]
+  imports: [MatButtonModule,MatInputModule,MatDividerModule],
+  exports: [MatButtonModule,MatInputModule,MatDividerModule]
 })
 export class MaterialAppModule { }
